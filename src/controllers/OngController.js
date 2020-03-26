0ee3da84254a93e7fcf7a2f7b1007238977fdc2c
patricklongo1 <ck.longo@bol.com.br>
2020-03-25 22:06:37 -0300

    done

---
+++ src/controllers/OngController.js
@@ -0,0 +1,50 @@
+import crypto from "crypto";
+import connection from "../database/connection";
+
+class OngController {
+  async index(req, res) {
+    const ongs = await connection("ongs").select("*");
+
+    return res.json(ongs);
+  }
+
+  async store(req, res) {
+    try {
+      const { name, email, whatsapp, city, uf } = req.body;
+
+      const ongExists = await connection("ongs")
+        .where("name", name)
+        .select("name")
+        .first();
+
+      if (ongExists) {
+        return res.status(400).json({ error: "This ONG already exists" });
+      }
+
+      const id = crypto.randomBytes(4).toString("HEX");
+
+      await connection("ongs").insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+      });
+
+      return res.json({ id });
+    } catch (error) {
+      return res.status(404).json({ message: "Bad Request" });
+    }
+  }
+
+  async delete(req, res) {
+    await connection("ongs")
+      .where("id", req.params.id)
+      .delete();
+
+    return res.json("Ong deletada");
+  }
+}
+
+export default new OngController();
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+import OngController from "./controllers/OngController";
+import IncidentController from "./controllers/IncidentController";
+import ProfileController from "./controllers/ProfileController";
+import SessionController from "./controllers/SessionController";
+
+const routes = Router();
+
+routes.post("/sessions", SessionController.store);
+
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.store);
+routes.delete("/ongs/:id", OngController.delete);
+
+routes.post("/incidents", IncidentController.store);
+routes.get("/incidents", IncidentController.index);
+routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.get("/profile", ProfileController.index);
+
+export default routes;
